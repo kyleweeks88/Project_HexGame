9bd9ed08afa895736a73d52a179e99c7a2f4ff9c
Kyle Weeks <60368775+kyleweeks88@users.noreply.github.com>
2021-12-10 13:36:13 -0800

    12.10 bump

--- Project_HexGame/Assets/Scripts/GlowHighlight.cs
+++ Project_HexGame/Assets/Scripts/GlowHighlight.cs
@@ -16,11 +16,14 @@ public class GlowHighlight : MonoBehaviour
 
     Color validSpaceColor = Color.green;
     Color originalGlowColor;
+    //Color validSpaceIntensity = new Color(2.5f, 2.5f, 1f);
+    //Color originalIntensity;
 
     private void Awake()
     {
         PrepareMaterialDictionaries();
         originalGlowColor = glowMat.GetColor("_GlowColor");
+        //originalIntensity = glowMat.GetColor("_EmissionColor");
     }
 
     public void ToggleGlow()
@@ -51,6 +54,7 @@ public class GlowHighlight : MonoBehaviour
             foreach (Material material in glowMaterialDict[renderer])
             {
                 material.SetColor("_GlowColor", originalGlowColor);
+                //material.SetColor("_EmissionColor", originalIntensity);
             }
         }
     }
@@ -64,6 +68,7 @@ public class GlowHighlight : MonoBehaviour
             foreach (Material material in glowMaterialDict[renderer])
             {
                 material.SetColor("_GlowColor", validSpaceColor);
+                //material.SetColor("_EmissionColor", validSpaceIntensity);
             }
         }
     }
